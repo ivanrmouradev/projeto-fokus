a9ae43f266363e58bd2f7e9ada8a1a15c32d5ec7
Ivan Moura <ivanrmouradev@gmail.com>
2025-10-20 11:30:03 -0300

    comentário adicionado em script

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Esse código é massa
+
 const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
